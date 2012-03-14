148c0c6be040516210e8c660b5d58513bdc3635b
Oyvind Myklatun <oyvinmy@gmail.com>
2012-03-14 11:41:32 +0100

    Added Labels, Textfields and Buttons for the GUI

---
+++ src/no/ntnu/fp/gui/AppointmentForm.java
@@ -0,0 +1,163 @@
+package no.ntnu.fp.gui;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.Date;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class AppointmentForm extends JFrame{
+	
+	private JPanel panel;
+	
+	private JLabel name;
+	private JTextField nameField;
+	
+	private JLabel place;
+	private JTextField placeField;
+	
+	private JLabel from;
+	private JTextField fromField;
+	
+	private JLabel to;
+	private JTextField toField;
+	
+	private JLabel description;
+	private JTextArea descriptionArea;
+	
+	private JLabel participants;
+	private JTextArea participantsArea;
+	
+	private JButton addParticipant;
+	
+	private JButton saveAppointment;
+	
+	public AppointmentForm (){
+		
+		panel = new JPanel();
+		
+		name = new JLabel("Navn: ");
+		nameField = new JTextField("", 20);
+		
+		place = new JLabel("Sted: ");
+		placeField = new JTextField("", 20);
+		
+		from = new JLabel("Fra: ");
+		fromField = new JTextField("", 10);
+		
+		to = new JLabel("Til: ");
+		toField = new JTextField("", 10);
+		
+		description = new JLabel("Beskrivelse:");
+		descriptionArea = new JTextArea(4, 20);
+		
+		participants = new JLabel("Deltagere: ");
+		participantsArea = new JTextArea(4, 20);
+		
+		addParticipant = new JButton("Legg til deltagere");
+		
+		saveAppointment = new JButton("Lagre");
+		
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.anchor = GridBagConstraints.WEST;
+		c.ipadx = 10;
+		c.ipady = 10;
+		c.gridx = 0;
+		c.gridy = 0;
+		panel.add(name, c);
+		
+		c.ipadx = 0;
+		c.ipady = 0;
+		c.gridx = 1;
+		c.gridy = 0;
+		panel.add(nameField, c);
+		
+		c.ipadx = 10;
+		c.ipady = 10;
+		c.gridx = 0;
+		c.gridy = 1;
+		panel.add(place, c);
+		
+		c.ipadx = 0;
+		c.ipady = 0;
+		c.gridx = 1;
+		c.gridy = 1;
+		panel.add(placeField, c);
+		
+		c.ipadx = 10;
+		c.ipady = 10;
+		c.gridx = 0;
+		c.gridy = 2;
+		panel.add(from, c);
+		
+		c.ipadx = 0;
+		c.ipady = 0;
+		c.gridx = 1;
+		c.gridy = 2;
+		panel.add(fromField, c);
+		
+		c.ipadx = 10;
+		c.ipady = 10;
+		c.gridx = 0;
+		c.gridy = 3;
+		panel.add(to, c);
+		
+		c.ipadx = 0;
+		c.ipady = 0;
+		c.gridx = 1;
+		c.gridy = 3;
+		panel.add(toField, c);
+		
+		c.ipadx = 10;
+		c.ipady = 10;
+		c.gridx = 0;
+		c.gridy = 4;
+		panel.add(description, c);
+		
+		c.ipadx = 10;
+		c.ipady = 10;
+		c.gridx = 1;
+		c.gridy = 4;
+		panel.add(descriptionArea, c);
+		
+		c.ipadx = 10;
+		c.ipady = 10;
+		c.gridx = 0;
+		c.gridy = 5;
+		panel.add(participants, c);
+		
+		c.ipadx = 10;
+		c.ipady = 10;
+		c.gridx = 1;
+		c.gridy = 5;
+		panel.add(participantsArea, c);
+		
+		
+		c.gridx = 0;
+		c.gridy = 6;
+		panel.add(addParticipant, c);
+		
+		c.gridx = 1;
+		c.gridy = 6;
+		panel.add(saveAppointment, c);
+		
+		this.add(panel);
+		
+		
+	}
+	public static void main(String[] args) {
+		AppointmentForm form = new AppointmentForm();
+		form.pack();
+		form.setVisible(true);
+	}
+	
+	
+	
+}
