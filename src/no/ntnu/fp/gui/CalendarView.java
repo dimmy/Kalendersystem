ce830c657ea708dca5cb5e5c8cd542d3eb32b0a8
David M <dimbnd@gmail.com>
2012-03-15 12:27:41 +0100

    Added arrows to the CalendarView

--- src/no/ntnu/fp/gui/CalendarView.java
+++ src/no/ntnu/fp/gui/CalendarView.java
@@ -1,5 +1,7 @@
 package no.ntnu.fp.gui;
 
+import java.awt.Color;
+
 import javax.swing.*;
 
 /**
@@ -10,8 +12,59 @@ import javax.swing.*;
  */
 
 public class CalendarView extends JPanel {
+
+	JButton btnPrevWeek;
+	JButton btnNextWeek;
+	JPanel innerCalendarView;
+
 	public CalendarView() {
 		// Placeholder
-		add(new JLabel("Calendar goes here (to be implemented)"));
+		btnPrevWeek = new JButton();
+		btnNextWeek = new JButton();
+		innerCalendarView = new JPanel();
+		innerCalendarView.setBackground(new Color(255, 128, 128));
+
+		btnPrevWeek.setIcon(new ImageIcon("Icons/leftarrow.png"));
+		btnNextWeek.setIcon(new ImageIcon("Icons/rightarrow.png"));
+		
+		SpringLayout spring = new SpringLayout();
+		setLayout(spring);
+
+		add(btnPrevWeek);
+		add(innerCalendarView);
+		add(btnNextWeek);
+
+		spring.putConstraint(SpringLayout.WEST, btnPrevWeek, 0,
+				SpringLayout.WEST, this);
+		spring.putConstraint(SpringLayout.EAST, btnNextWeek, 0,
+				SpringLayout.EAST, this);
+
+		spring.putConstraint(SpringLayout.WEST, innerCalendarView, 5,
+				SpringLayout.EAST, btnPrevWeek);
+		spring.putConstraint(SpringLayout.EAST, innerCalendarView, -5,
+				SpringLayout.WEST, btnNextWeek);
+
+		spring.putConstraint(SpringLayout.NORTH, innerCalendarView, 0,
+				SpringLayout.NORTH, this);
+		spring.putConstraint(SpringLayout.SOUTH, innerCalendarView, 0,
+				SpringLayout.SOUTH, this);
+
+		Spring middle = Spring.scale(spring.getConstraints(innerCalendarView).getHeight(),
+				0.5f);
+		Spring halfButton = Spring.scale(spring.getConstraints(btnPrevWeek)
+				.getHeight(), 0.5f);
+		Spring btnVertPos = Spring.sum(middle, Spring.minus(halfButton));
+		spring.getConstraints(btnPrevWeek).setY(btnVertPos);
+		spring.getConstraints(btnNextWeek).setY(btnVertPos);
 	}
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Calendar view test");
+		frame.add(new CalendarView());
+		frame.setVisible(true);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 300);
+	}
+
 }
--- src/no/ntnu/fp/gui/EditMeetingOption.java
+++ src/no/ntnu/fp/gui/EditMeetingOption.java
@@ -1,31 +1,32 @@
 package no.ntnu.fp.gui;
-	import java.awt.GridBagLayout;
-	import javax.swing.BorderFactory;
-	import javax.swing.JFrame;
-	import javax.swing.JOptionPane;
-	import javax.swing.JPanel;
+
+import java.awt.GridBagLayout;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class EditMeetingOption extends JPanel {
 
-		private JOptionPane text;
-		
-		public static void main (String []args){
-			JFrame editmeeting = new JFrame("Endre møte?");
-			editmeeting.setSize(260,140);
-			editmeeting.getContentPane().add(new EditMeetingOption());
-			editmeeting.setLocationRelativeTo(null);
-			editmeeting.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			editmeeting.setVisible(true);
+	private JOptionPane text;
+
+	public static void main(String[] args) {
+		JFrame editmeeting = new JFrame("Endre møte?");
+		editmeeting.setSize(260, 140);
+		editmeeting.getContentPane().add(new EditMeetingOption());
+		editmeeting.setLocationRelativeTo(null);
+		editmeeting.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		editmeeting.setVisible(true);
 	}
 
-		public EditMeetingOption(){
-			setLayout(new GridBagLayout());
-			setBorder(BorderFactory.createEmptyBorder(7, 7, 7, 7));
-			
-			text = new JOptionPane("Er du sikker du vil endre møtet?", JOptionPane.INFORMATION_MESSAGE, JOptionPane.YES_NO_OPTION);
-			add(text);
-		
-		}
-	
+	public EditMeetingOption() {
+		setLayout(new GridBagLayout());
+		setBorder(BorderFactory.createEmptyBorder(7, 7, 7, 7));
+
+		text = new JOptionPane("Er du sikker du vil endre møtet?",
+				JOptionPane.INFORMATION_MESSAGE, JOptionPane.YES_NO_OPTION);
+		add(text);
+
+	}
 
 }
