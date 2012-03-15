7ee63f3b7f3e0bc598948228d2734bb1d4dadfc9
David M <dimbnd@gmail.com>
2012-03-15 10:09:11 +0100

    Further renames and changes

--- src/no/ntnu/fp/gui/DeleteMeetingOption.java
+++ src/no/ntnu/fp/gui/DeleteMeetingOption.java
@@ -7,24 +7,24 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class deletemeetingGUI extends JPanel{
+public class DeleteMeetingOption extends JPanel{
 
 	private JOptionPane text;
 	
 	public static void main (String []args){
-		JFrame deletemeeting = new JFrame("Slette møte?");
+		JFrame deletemeeting = new JFrame("Slette mÃ¸te?");
 		deletemeeting.setSize(260,140);
-		deletemeeting.getContentPane().add(new deletemeetingGUI());
+		deletemeeting.getContentPane().add(new DeleteMeetingOption());
 		deletemeeting.setLocationRelativeTo(null);
 		deletemeeting.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		deletemeeting.setVisible(true);
 }
 
-	public deletemeetingGUI(){
+	public DeleteMeetingOption(){
 		setLayout(new GridBagLayout());
 		setBorder(BorderFactory.createEmptyBorder(7, 7, 7, 7));
 		
-		text = new JOptionPane("Er du sikker du vil slette møtet?", JOptionPane.INFORMATION_MESSAGE, JOptionPane.YES_NO_OPTION);
+		text = new JOptionPane("Er du sikker du vil slette mÃ¸tet?", JOptionPane.INFORMATION_MESSAGE, JOptionPane.YES_NO_OPTION);
 		add(text);
 	
 	}
--- src/no/ntnu/fp/gui/EditMeetingOption.java
+++ src/no/ntnu/fp/gui/EditMeetingOption.java
@@ -5,24 +5,24 @@ package no.ntnu.fp.gui;
 	import javax.swing.JOptionPane;
 	import javax.swing.JPanel;
 
-public class editmeetingBoxGUI extends JPanel {
+public class EditMeetingOption extends JPanel {
 
 		private JOptionPane text;
 		
 		public static void main (String []args){
-			JFrame editmeeting = new JFrame("Endre møte?");
+			JFrame editmeeting = new JFrame("Endre mÃ¸te?");
 			editmeeting.setSize(260,140);
-			editmeeting.getContentPane().add(new editmeetingBoxGUI());
+			editmeeting.getContentPane().add(new EditMeetingOption());
 			editmeeting.setLocationRelativeTo(null);
 			editmeeting.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			editmeeting.setVisible(true);
 	}
 
-		public editmeetingBoxGUI(){
+		public EditMeetingOption(){
 			setLayout(new GridBagLayout());
 			setBorder(BorderFactory.createEmptyBorder(7, 7, 7, 7));
 			
-			text = new JOptionPane("Er du sikker du vil endre møtet?", JOptionPane.INFORMATION_MESSAGE, JOptionPane.YES_NO_OPTION);
+			text = new JOptionPane("Er du sikker du vil endre mÃ¸tet?", JOptionPane.INFORMATION_MESSAGE, JOptionPane.YES_NO_OPTION);
 			add(text);
 		
 		}
