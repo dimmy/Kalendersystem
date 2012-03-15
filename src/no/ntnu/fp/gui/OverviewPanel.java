8a133cc3c46496fdf11b3350395fa07ddaa7cfc4
David M <dimbnd@gmail.com>
2012-03-15 10:59:54 +0100

    Cleaned up Overview layout

--- src/no/ntnu/fp/gui/OverviewPanel.java
+++ src/no/ntnu/fp/gui/OverviewPanel.java
@@ -17,6 +17,9 @@ import javax.swing.*;
 
 public class OverviewPanel extends JPanel {
 
+	static int buttonVerticalPadding = 10;
+	static int buttonHorizontalPadding = 5;
+
 	JButton btnNewAppointment;
 	JButton btnAddCalendar;
 	JButton btnInbox;
@@ -38,42 +41,74 @@ public class OverviewPanel extends JPanel {
 
 		// Left side button panel
 
+		SpringLayout spring = new SpringLayout();
+
 		panLeftButtonPanel = new JPanel();
-		panLeftButtonPanel.setLayout(new BoxLayout(panLeftButtonPanel,
-				BoxLayout.Y_AXIS));
+		panLeftButtonPanel.setLayout(spring);
 
 		panLeftButtonPanel.add(btnNewAppointment);
 		panLeftButtonPanel.add(btnAddCalendar);
 		panLeftButtonPanel.add(btnInbox);
 		panLeftButtonPanel.add(btnLogOut);
 
+		spring.putConstraint(SpringLayout.WEST, btnNewAppointment,
+				buttonHorizontalPadding, SpringLayout.WEST, panLeftButtonPanel);
+		spring.putConstraint(SpringLayout.WEST, btnAddCalendar,
+				buttonHorizontalPadding, SpringLayout.WEST, panLeftButtonPanel);
+		spring.putConstraint(SpringLayout.WEST, btnInbox,
+				buttonHorizontalPadding, SpringLayout.WEST, panLeftButtonPanel);
+		spring.putConstraint(SpringLayout.WEST, btnLogOut,
+				buttonHorizontalPadding, SpringLayout.WEST, panLeftButtonPanel);
+
+		Spring rightBorder = Spring.constant(0);
+		rightBorder = rightBorder.max(rightBorder,
+				spring.getConstraints(btnNewAppointment).getWidth());
+		rightBorder = rightBorder.max(rightBorder,
+				spring.getConstraints(btnAddCalendar).getWidth());
+		rightBorder = rightBorder.max(rightBorder,
+				spring.getConstraints(btnInbox).getWidth());
+		rightBorder = rightBorder.max(rightBorder,
+				spring.getConstraints(btnLogOut).getWidth());
+
+		spring.getConstraints(btnNewAppointment).setWidth(rightBorder);
+		spring.getConstraints(btnAddCalendar).setWidth(rightBorder);
+		spring.getConstraints(btnInbox).setWidth(rightBorder);
+		spring.getConstraints(btnLogOut).setWidth(rightBorder);
+
+		rightBorder = Spring.sum(rightBorder, Spring.constant(10));
+
+		spring.getConstraints(panLeftButtonPanel).setWidth(rightBorder);
+
+		spring.putConstraint(SpringLayout.NORTH, btnNewAppointment,
+				buttonVerticalPadding, SpringLayout.NORTH, panLeftButtonPanel);
+		spring.putConstraint(SpringLayout.NORTH, btnAddCalendar,
+				buttonVerticalPadding, SpringLayout.SOUTH, btnNewAppointment);
+		spring.putConstraint(SpringLayout.NORTH, btnInbox,
+				buttonVerticalPadding, SpringLayout.SOUTH, btnAddCalendar);
+		spring.putConstraint(SpringLayout.NORTH, btnLogOut,
+				buttonVerticalPadding, SpringLayout.SOUTH, btnInbox);
+
 		// Title
 
-		lblWeekDisplay = new JLabel("<html><h1>Uke ?</h1></html>");
-		lblLoginInfo = new JLabel("Logget inn som: <brukernavn>");
-		lblLoginInfo.setHorizontalTextPosition(JLabel.RIGHT);
-		
-		panTitle = new JPanel();
-		panTitle.setLayout(new GridBagLayout());
-		
-		GridBagConstraints c = new GridBagConstraints();
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 0;
-		c.gridy = 0;
-		panTitle.add(lblLoginInfo, c);
-		c.gridy = 1;
-		panTitle.add(lblWeekDisplay, c);
+		lblWeekDisplay = new JLabel("<html><h1>Uke ?</h1></html>", SwingConstants.CENTER);
+		lblLoginInfo = new JLabel("<html>Logget inn som: brukernavn</html>", SwingConstants.RIGHT);
 
-		panTitle.setBackground(new Color(255, 128, 128)); // color for testing
+		panTitle = new JPanel();
+		panTitle.setLayout(new BoxLayout(panTitle, BoxLayout.Y_AXIS));
+
+		panTitle.add(lblLoginInfo);
+		panTitle.add(lblWeekDisplay);
+
+		lblLoginInfo.setOpaque(true);
+		lblWeekDisplay.setOpaque(true);
 
 		// Calendar view
 
 		calendarView = new CalendarView();
-		calendarView.setBackground(new Color(128, 255, 128));
 
 		// Combine!
 
-		SpringLayout spring = new SpringLayout();
+		spring = new SpringLayout();
 
 		setLayout(spring);
 
@@ -96,8 +131,8 @@ public class OverviewPanel extends JPanel {
 		spring.putConstraint(SpringLayout.SOUTH, panLeftButtonPanel, 0,
 				SpringLayout.SOUTH, this);
 		// PS: This line sets the width of the button panel
-		spring.putConstraint(SpringLayout.EAST, panLeftButtonPanel, 200,
-				SpringLayout.WEST, this);
+		// spring.putConstraint(SpringLayout.EAST, panLeftButtonPanel, 200,
+		// SpringLayout.WEST, this);
 		spring.putConstraint(SpringLayout.WEST, panLeftButtonPanel, 0,
 				SpringLayout.WEST, this);
 
