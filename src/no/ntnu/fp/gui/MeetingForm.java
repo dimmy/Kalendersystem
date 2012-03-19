ab75d09e10529f3f9784e3f3139ad4ad268950d7
Oyvind Myklatun <oyvinmy@gmail.com>
2012-03-19 10:32:31 +0100

    Added ActionListener to RoomButton and addParticipantButton

--- src/no/ntnu/fp/gui/MeetingForm.java
+++ src/no/ntnu/fp/gui/MeetingForm.java
@@ -1,13 +1,17 @@
 package no.ntnu.fp.gui;
 
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -39,13 +43,15 @@ public class MeetingForm extends JFrame{
 	
 	private JLabel description;
 	private JTextArea descriptionArea;
+	private JScrollPane descriptionScroll;
 	
 	private JLabel participants;
 	private JTextArea participantsArea;
+	private JScrollPane participantScroll;
 	
 	private JButton addParticipant;
-	
 	private JButton saveMeeting;
+	private JButton roomButton;
 	
 	public MeetingForm (){
 		
@@ -69,14 +75,50 @@ public class MeetingForm extends JFrame{
 		dateField = new JTextField("", 10);
 		
 		description = new JLabel("Beskrivelse:");
-		descriptionArea = new JTextArea(4, 20);
+		descriptionArea = new JTextArea();
+		descriptionScroll = new JScrollPane(descriptionArea);
+		descriptionScroll.setPreferredSize(new Dimension(200,100));
+		descriptionArea.setWrapStyleWord(true);
+		descriptionArea.setLineWrap(true);
 		
 		participants = new JLabel("Deltagere: ");
-		participantsArea = new JTextArea(4, 20);
+		participantsArea = new JTextArea();
+		participantScroll = new JScrollPane(participantsArea);
+		participantScroll.setPreferredSize(new Dimension(200,100));
+		participantsArea.setWrapStyleWord(true);
+		participantsArea.setLineWrap(true);
+		
 		
 		addParticipant = new JButton("Legg til deltagere");
+		addParticipant.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				ChooseParticipantPanel participantChooser = new ChooseParticipantPanel();
+				JFrame frame = new JFrame();
+				frame.add(participantChooser);
+				frame.pack();
+				frame.setVisible(true);
+			}
+		});
+		
 		
 		saveMeeting = new JButton("Lagre");
+		
+		
+		roomButton = new JButton("Room");
+		roomButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				PlaceRoomChooserPanel roomChooser = new PlaceRoomChooserPanel();
+				JFrame frame = new JFrame();
+				frame.add(roomChooser);
+				frame.pack();
+				frame.setVisible(true);
+			}
+		});
+		
 		
 		panel.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
@@ -89,103 +131,83 @@ public class MeetingForm extends JFrame{
 		c.gridy = 0;
 		panel.add(name, c);
 		
-		c.gridwidth = 4;
-		c.ipadx = 0;
-		c.ipady = 0;
-		c.gridx = 1;
-		c.gridy = 0;
-		panel.add(nameField, c);
-		
 		c.gridwidth = 1;
-		c.ipadx = 10;
-		c.ipady = 10;
 		c.gridx = 0;
 		c.gridy = 1;
 		panel.add(place, c);
 		
-		c.gridwidth = 4;
-		c.ipadx = 0;
-		c.ipady = 0;
-		c.gridx = 1;
-		c.gridy = 1;
-		panel.add(placeField, c);
-		
 		c.gridwidth = 1;
-		c.ipadx = 10;
-		c.ipady = 10;
 		c.gridx = 0;
 		c.gridy = 2;
 		panel.add(time, c);
 		
 		c.gridwidth = 1;
-		c.ipadx = 10;
-		c.ipady = 10;
 		c.gridx = 1;
 		c.gridy = 2;
 		panel.add(from, c);
 		
 		c.gridwidth = 1;
-		c.ipadx = 0;
-		c.ipady = 0;
-		c.gridx = 2;
-		c.gridy = 2;
-		panel.add(fromField, c);
-		
-		c.gridwidth = 1;
-		c.ipadx = 10;
-		c.ipady = 10;
 		c.gridx = 3;
 		c.gridy = 2;
 		panel.add(to, c);
 		
 		c.gridwidth = 1;
-		c.ipadx = 0;
-		c.ipady = 0;
-		c.gridx = 4;
-		c.gridy = 2;
-		panel.add(toField, c);
-		
-		c.gridwidth = 1;
-		c.ipadx = 10;
-		c.ipady = 10;
 		c.gridx = 0;
 		c.gridy = 3;
 		panel.add(date, c);
 		
-		c.gridwidth = 4;
+		c.gridwidth = 1;
 		c.ipadx = 0;
 		c.ipady = 0;
-		c.gridx = 1;
-		c.gridy = 3;
-		panel.add(dateField, c);
-		
-		c.gridwidth = 1;
-		c.ipadx = 10;
-		c.ipady = 10;
 		c.gridx = 0;
 		c.gridy = 4;
 		panel.add(description, c);
 		
 		c.gridwidth = 4;
-		c.ipadx = 10;
-		c.ipady = 10;
 		c.gridx = 1;
 		c.gridy = 4;
-		panel.add(descriptionArea, c);
+		panel.add(descriptionScroll, c);
 		
 		c.gridwidth = 1;
-		c.ipadx = 10;
-		c.ipady = 10;
 		c.gridx = 0;
 		c.gridy = 5;
 		panel.add(participants, c);
 		
 		c.gridwidth = 4;
-		c.ipadx = 10;
-		c.ipady = 10;
 		c.gridx = 1;
 		c.gridy = 5;
-		panel.add(participantsArea, c);
+		panel.add(participantScroll, c);
+		
+		c.gridwidth = 4;
+		c.gridx = 1;
+		c.gridy = 0;
+		panel.add(nameField, c);
+		
+		c.gridwidth = 4;
+		c.gridx = 1;
+		c.gridy = 1;
+		panel.add(placeField, c);
+		
+		c.gridwidth = 3;
+		c.gridx = 5;
+		c.gridy = 1;
+		panel.add(roomButton, c);
+		
+		
+		c.gridwidth = 1;
+		c.gridx = 2;
+		c.gridy = 2;
+		panel.add(fromField, c);
+		
+		c.gridwidth = 1;
+		c.gridx = 4;
+		c.gridy = 2;
+		panel.add(toField, c);
+		
+		c.gridwidth = 4;
+		c.gridx = 1;
+		c.gridy = 3;
+		panel.add(dateField, c);
 		
 		c.gridwidth = 2;
 		c.gridx = 0;
