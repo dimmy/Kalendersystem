c573884b31e3e64950091a57204025ea3e909337
Hedda <hedda.nonstad@gmail.com>
2012-03-23 12:15:44 +0100

    Hashing to login function

---
+++ src/no/ntnu/fp/model/Tools.java
@@ -0,0 +1,29 @@
+package no.ntnu.fp.model;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Tools {
+	
+
+		 public static String SHAHash(String code) throws NoSuchAlgorithmException {
+		 
+		     MessageDigest md = MessageDigest.getInstance("SHA-256");
+		  md.update(code.getBytes());
+		  
+		  byte byteData[] = md.digest();
+		  
+		  //convert the byte to hex format method 1
+		      StringBuffer sb = new StringBuffer();
+		      for (int i = 0; i < byteData.length; i++) {
+		       sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+		      }
+		  
+		   return sb.toString();
+		 }
+		 
+		 public static void main(String[] args) throws NoSuchAlgorithmException{
+		  System.out.println(SHAHash("yeah"));
+		 }
+	}
+
