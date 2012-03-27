cbf470cdb4809399b13285599e84bb1b870474b4
Oyvind Myklatun <oyvinmy@gmail.com>
2012-03-27 09:49:42 +0200

    Tried to convert a Event to xml and send to server

--- src/no/ntnu/fp/serv/Client.java
+++ src/no/ntnu/fp/serv/Client.java
@@ -2,6 +2,7 @@ package no.ntnu.fp.serv;
 
 import java.io.*;
 import java.net.*;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +13,9 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
 import no.ntnu.fp.model.Event;
+import no.ntnu.fp.model.User;
 import no.ntnu.fp.model.ref.UserRef;
+import no.ntnu.fp.storage.ClassToXMLConverter;
 import nu.xom.Document;
 import nu.xom.Element;
 
@@ -25,10 +28,10 @@ public class Client {
 		XMLInputFactory inputFactory = null;
 		XMLStreamReader xmlReader = null;
 		
-		
-		String en = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-		String to = "<getevents username=\"test\">";
-		String tre = "</getevents>";
+		Event event = new Event(0,"fdsfds", "fdsfds", new Date(22), 2, "appointment", "dfsfds", "dfsadsa", "dsadsa", "test");
+		List<User> users = new ArrayList<User>();
+		users.add(new User("per", "per", "", 9));
+		users.add(new User("per", "per", "", 9));
 //		String to = "<changeevent eventid=\"7\" eventdescription=\"Laangt\" place=\"Knuts kontor\" timelength = \"110\" name=\"personalfode\" leader=\"test\" type=\"appointment\">";
 //		String tre = "<addparticipants eventid = \"7\">";
 //		String fire = "<user username = \"perry\">";
@@ -37,11 +40,11 @@ public class Client {
 //		String sju = "</user>";
 //		String otte = "</addparticipants>";
 //		String ni = "</changeevent>";
-
+		List<String> meeting = ClassToXMLConverter.EventFromClassToXML(event, users);
+		
 		try {
 			kkSocket = new Socket("Oyvind-PC", 4444);
 			out = new PrintWriter(kkSocket.getOutputStream(), true);
-			
 		} catch (UnknownHostException e) {
 			System.err.println("Don't know about host: Oyvind-PC.");
 			System.exit(1);
@@ -51,28 +54,27 @@ public class Client {
 			System.exit(1);
 		}
 		
-		out.print(en);
-		out.print(to);
-		out.print(tre);
-		
-		XMLInputFactory factory = XMLInputFactory.newInstance();
-		xmlReader = factory.createXMLStreamReader(kkSocket.getInputStream());
-		while(xmlReader.hasNext()){
-			switch (xmlReader.getEventType()) {
-			case XMLStreamConstants.END_DOCUMENT:
-				break;
-			case XMLStreamConstants.START_ELEMENT:
-				if(xmlReader.getName().toString() == "login"){
-					
-				}
-				
-				break;
-			case XMLStreamConstants.START_DOCUMENT:
-				break;
-
-			}
-			xmlReader.next();
+		for (int i = 0; i < meeting.size(); i++) {
+			out.println(meeting.get(i));
 		}
-		xmlReader.close();
+		
+//		XMLInputFactory factory = XMLInputFactory.newInstance();
+//		xmlReader = factory.createXMLStreamReader(kkSocket.getInputStream());
+//		while(xmlReader.hasNext()){
+//			switch (xmlReader.getEventType()) {
+//			case XMLStreamConstants.END_DOCUMENT:
+//				break;
+//			case XMLStreamConstants.START_ELEMENT:
+//				if(xmlReader.getName().toString() == "login"){
+//					
+//				}
+//				
+//				break;
+//			case XMLStreamConstants.START_DOCUMENT:
+//				break;
+//
+//			}
+//			xmlReader.next();
+//		}
 	}
 }
